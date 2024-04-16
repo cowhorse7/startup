3060ed2d791026144cadfbbff5b19854b4620dbe
cowhorse7 <becca11175@gmail.com>
2024-04-16 15:00:29 -0600

    headerApp

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className='body bg-dark text-light'>App will display here</div>;
+  return (
+  <div className='body bg-dark'>
+      <header>
+            <h1>Bouquet<img height="120px" src="sparkles.png" alt="navy blue sparkles"/></h1>
+            </header>
+            <nav>
+                <menu>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="create.html">Create</a></li>
+                    <li><a href="arrangements.html"> Arrangements</a></li>
+                    <li><a href="about.html">About</a></li>
+                </menu>
+            </nav>
+        <main>App comps go here</main>
+        <footer>
+            <span class = "text-reset">Rebecca Case &nbsp;</span>
+            <a href="https://github.com/cowhorse7/startup">GitHub</a>
+        </footer>
+  </div>
+  );
 }
